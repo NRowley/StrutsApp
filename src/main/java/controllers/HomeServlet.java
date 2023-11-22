d1b59e7aa7f793179e85b467419bd759c21bffae
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-22 16:07:16 -0600

    need to resolve issues with tomcat

--- src/main/java/controllers/HomeServlet.java
+++ src/main/java/controllers/HomeServlet.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.HomeServlet", urlPatterns = "/")
+@WebServlet(name = "controllers.HomeServlet", urlPatterns = {"/"})
 public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+//        req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().println("test");
     }
 }
